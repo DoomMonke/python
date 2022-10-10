301e252c6d98dae1816c6bcc999663d918b849af
DoomMonke <115462632+DoomMonke@users.noreply.github.com>
2022-10-10 13:21:30 +0300

    Add files via upload

---
+++ kilpkonn_01.py
@@ -0,0 +1,53 @@
+#Alari Viilma
+#10.10.22
+# Harjutame kilpkonna
+import turtle
+
+#tekitame akna
+aken = turtle.Screen()
+aken.setup(300,300)
+aken.title("Ülesanne 1 - MM")
+
+k1 = turtle.Turtle()
+k2 = turtle.Turtle()
+k3 = turtle.Turtle()
+k4 = turtle.Turtle()
+k5 = turtle.Turtle()
+k6 = turtle.Turtle()
+k7 = turtle.Turtle()
+k8 = turtle.Turtle()
+
+k1.color("pink")
+k2.color("red")
+k3.color("purple")
+k4.color("blue")
+k5.color("orange")
+k6.color("yellow")
+k7.color("green")
+k8.color("lime")
+
+k1.fd(100)
+k2.left(45)
+k2.fd(100)
+k3.left(90)
+k3.fd(100)
+k4.left(135)
+k4.fd(100)
+k5.left(180)
+k5.fd(100)
+k6.left(-45)
+k6.fd(100)
+k7.left(-135)
+k7.fd(100)
+k8.left(-90)
+k8.fd(100)
+
+
+
+
+
+
+#suleb akna klikkides
+turtle.exitonclick()
+
+
